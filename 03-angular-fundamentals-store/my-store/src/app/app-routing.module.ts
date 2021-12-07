4e66d6b9b714126a254ddd81afef5777fdb6a280
Rodrigo Ribeiro Dias <rrd.rodrigodias@gmail.com>
2021-12-07 15:21:15 -0300

    Finish navigation flow

--- 03-angular-fundamentals-store/my-store/src/app/app-routing.module.ts
+++ 03-angular-fundamentals-store/my-store/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductItemDetailComponent } from './product-item-detail/product-item-detail.component';
 import { CartComponent } from './cart/cart.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
 
 const routes: Routes = [
   { path: '', component: ProductListComponent },
   { path: 'product/:id', component: ProductItemDetailComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'confirmation', component: ConfirmationComponent}
 ];
 
 @NgModule({
